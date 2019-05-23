aa3c987d9ff565e6e01cb4873741d3414145f272
Carlos Eduardo Garcia Estrada <42812352+Tlaloc15@users.noreply.github.com>
2019-05-23 14:19:24 -0500

    Formulario y Botón Eliminar

--- Agenda-App/Agenda.js
+++ Agenda-App/Agenda.js
@@ -7,24 +7,68 @@ export default class Agenda{
         this._tablaIncio();
     }
 
-    _tablaIncio() {}
+    _tablaIncio() {
+      let lsContacto = JSON.parse(localStorage.getItem("contacto"));
+        if (lsContacto === null) {
+            return;
+        }
+        lsContacto.forEach((c, index) => {
 
-    _AgregarTa(contacto) {
+            ///if (e.taller === this._nameTaller || e.taller === undefined) {
+                c.cumple = new Date(c.cumple);
+
+               this._agregarTa(new Contacto(c));
+        });
+    }
+
+
+    _quitarFila(fila, contacto){
+      let btnQuitar = document.createElement('input');
+      btnQuitar.type = "button";
+      btnQuitar.value = "Eliminar";
+      btnQuitar.className = "btn btn-danger"
+      btnQuitar.addEventListener('click', () => {
+      this._contactos.splice(contacto, 1);
+            fila.innerHTML = "";
+            localStorage.setItem("contacto", JSON.stringify(this._contactos));
+
+            return;
+      });
+      fila.cells[4].innerHTML = "";
+      fila.cells[4].appendChild(btnQuitar);
+    }
+
+    _agregarTa(contacto) {
         let fila = this._tablaAgenda.insertRow(-1);
 
-        let celNombre = fila.insertRow(0);
-        let celCorreo = fila.insertRow(1);
-        let celCumple = fila.insertRow(2);
+        let celNombre = fila.insertCell(0);
+        let celCorreo = fila.insertCell(1);
+        let celCumple = fila.insertCell(2);
+        let celEdad = fila.insertCell(3);
+
+        fila.insertCell(4);
 
         celNombre.innerHTML = contacto.nombre;
         celCorreo.innerHTML = contacto.correo;
         celCumple.innerHTML = contacto.obCumpleS();
+        celEdad.innerHTML = contacto.obEdad();
+
+
+        this._quitarFila(fila, contacto)
+
+       let objContacto = {
+          nombre: contacto.nombre,
+          correo: contacto.correo,
+          cumple: contacto.cumple
+        };
+
+        this._contactos.push(objContacto);
     }
 
     _buscar(correo) {
         let lugar = -1;
     
-        this._employees.forEach((c, index) => {
+        this._contactos.forEach((c, index) => {
           if(c.correo === correo) {
             lugar = index;
             return;
@@ -37,7 +81,7 @@ export default class Agenda{
     
     
     
-      AgregarCo(contacto) {
+      agregarCo(contacto) {
         let buscar = this._buscar(contacto.correo);
     
         if(buscar >= 0) {
@@ -50,7 +94,7 @@ export default class Agenda{
         }
     
     
-        this._AgregarTa(contacto);
+        this._agregarTa(contacto);
         localStorage.setItem("contacto", JSON.stringify(this._contactos));
       }
 }
--- Agenda-App/main.js
+++ Agenda-App/main.js
@@ -1,4 +1,5 @@
 import Contacto from "./contactos.js";
+import Agenda from "./Agenda.js";
 
 class Main{
     constructor() {
@@ -6,10 +7,10 @@ class Main{
         document.querySelector('#btnAgregar').addEventListener('click', () => {
             let form = document.querySelector('#form');
 
-            if(form.checkValidaty() === true) {
+            if (form.checkValidity() === true) {
                 let nombre = document.querySelector('#name').value;
                 let correo = document.querySelector('#correo').value;
-                let sCumple = document.querySelector('#cumpleaños').value;
+                let sCumple = document.querySelector('#cumpleanios').value;
                 sCumple = sCumple.split('-');
 
                 let cumple = new Date(sCumple[0], sCumple[1], sCumple[2]);
@@ -22,9 +23,10 @@ class Main{
 
                 let contacto = new Contacto(objContacto);
 
-                contacto.
+                Agenda2.agregarCo(contacto);
 
             }
+            form.classList.add("was-validated");
             
            })
 
